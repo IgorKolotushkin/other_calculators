4338e71ebb313f195cad2aebe2fef2feb789584e
IgorKolotushkin <dr_kia@mail.ru>
2023-11-12 11:52:07 +0300

    added history

--- templates/js/sum.js
+++ templates/js/sum.js
@@ -1,7 +1,23 @@
 function calc_duty() {
-    var salary = document.getElementById('salary').value
-    // let hrs = document.querySelector('.form-check-input').checked;
-    var hrs = document.querySelector('.form-check-input:checked').value;
-    document.getElementById('id_for_salary').innerHTML = salary;
-    document.getElementById('result').innerHTML = 'Результат: ' + hrs
+    const hours = +document.getElementById('hours').value;
+    const salary = +document.getElementById('salary').value / hours;
+    const bonus = +document.getElementById('bonus').value / hours;
+    const vmp = +document.getElementById('vmp').value / hours;
+    const sum_night = +document.getElementById('sum_night').value;
+    const hours_hight = +document.getElementById('hours_night').value;
+    const academic_degree = 3000 / hours;
+    let category = salary * 0.22 / hours;
+    let disutility = salary * 0.08 / hours;
+    
+    let hrs_duty = +document.querySelector('.form-check-input:checked').value;
+
+    let all_salary = ((salary * 2) + bonus + vmp + academic_degree + category + disutility) * hrs_duty;
+    let all_night = ((sum_night / hours_hight) * 8);
+    
+    let all_sum =  all_salary + all_night;
+    let result = all_sum - (all_sum * 0.13);
+    
+    alert('Результат: ' + result.toFixed(1));
+
+    document.getElementById('result').innerHTML = 'Результат: ' + result.toFixed(1);
 };
